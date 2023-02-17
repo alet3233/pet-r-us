ebcfb0271e0ba91df4975e1b200c04ff5bff192c
Alex Thomas <alet3233@gmail.com>
2023-02-16 19:37:15 -0600

    Part 4 of Pet R Us, customers page

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 // setting up path for EJS views
 app.set('views', path.join(__dirname, 'views'));
 // setting the views engine to ejs
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 // setting up express url encode
 app.use(express.urlencoded({ extended: true }));
 // setting up application type to be JSON
@@ -41,27 +41,31 @@ mongoose.connect(CONN).then(() => {
 
 // taking the resuable items as global variables
 let navItems = [
-        {
-            itemName: "Home",
-            itemLink: "/"
-        },
-        {
-            itemName: "Grooming",
-            itemLink: "/grooming"
-        },
-        {
-            itemName: "Boarding",
-            itemLink: "/boarding"
-        },
-        {
-            itemName: "Training",
-            itemLink: "/training"
-        },
-        {
-            itemName: "Register",
-            itemLink: "/register"
-        }
-    ]
+    {
+        itemName: "Home",
+        itemLink: "/"
+    },
+    {
+        itemName: "Grooming",
+        itemLink: "/grooming"
+    },
+    {
+        itemName: "Boarding",
+        itemLink: "/boarding"
+    },
+    {
+        itemName: "Training",
+        itemLink: "/training"
+    },
+    {
+        itemName: "Register",
+        itemLink: "/register"
+    },
+    {
+        itemName: "Customers",
+        itemLink: "/customers"
+    }
+]
 let homeData = {
     navItems,
     title: 'Pets R Us',
@@ -95,12 +99,12 @@ let homeData = {
 }
 
 // setting up route for index page and passing the relevant data to the index file
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render("index", homeData)
 })
 
 // setting up route for grooming page and passing the relevant data to the grooming file
-app.get('/grooming',(req,res)=>{
+app.get('/grooming', (req, res) => {
     res.render("grooming", {
         navItems,
         title: "Grooming",
@@ -109,27 +113,27 @@ app.get('/grooming',(req,res)=>{
             {
                 packageName: "Basic",
                 price: "29",
-                servicesName: [ "Monthly shower", "Bi-monthly haircut", "Special request available"]
+                servicesName: ["Monthly shower", "Bi-monthly haircut", "Special request available"]
             },
             {
                 packageName: "Standard",
                 price: "55",
-                servicesName: [ "Two showers a month", "Monthly haircut", "Special request available"]
+                servicesName: ["Two showers a month", "Monthly haircut", "Special request available"]
             },
             {
                 packageName: "Premium",
                 price: "100",
-                servicesName: [ "Weekly showers", "Two haircuts a month", "Special request available"]
+                servicesName: ["Weekly showers", "Two haircuts a month", "Special request available"]
             }
         ]
-           
+
     })
 })
 
 // setting up route for training page and passing the relevant data to the training file
-app.get('/training',(req,res)=>{
+app.get('/training', (req, res) => {
     res.render("training", {
-        navItems, 
+        navItems,
         title: "Training",
         description: "At Pets-R-Us we take pride in seeing pets learn new tricks and become the type of pet their owner wants them to be.  Whether you want your pet to learn to fetch, roll over, or become potty trained we have trainers with multiple decades of experience that can teach your loved one to reach the potential it can.  Our trainers have worked with all sort of dog and cat breeds and have over 20 plus years of collective experience.  We also specialize in teaching dogs tricks that would qualify them for shows.  We take an individualize approached to our training based on age, breed, temperament, personality and other aspects to come up with a plan of action for your pet to reach its full potential.  We understand each pet is a loved one and we take pride in our care of your pet in helping it grow into the type of pet the owner envisions.   ",
         heroImage: "images/panda.png",
@@ -139,34 +143,35 @@ app.get('/training',(req,res)=>{
         servicesList: [
             {
                 packageName: "Basic Tier",
-                servicesName: [ "Basic Training 25$ per classx"]
+                servicesName: ["Basic Training 25$ per classx"]
             },
             {
                 packageName: "Standard Tier",
-                servicesName: [ "Standard training 35$ per class"]
+                servicesName: ["Standard training 35$ per class"]
             },
             {
                 packageName: "Premium Tier",
-                servicesName: [ "Premium Training 50$ per class"]
+                servicesName: ["Premium Training 50$ per class"]
             }
         ]
-           
+
     })
 })
 
 // setting up route for boarding page and passing the relevant data to the boarding file
-app.get('/boarding',(req,res)=>{
+app.get('/boarding', (req, res) => {
     res.render("boarding", {
         navItems,
         title: 'Pets R Us',
         description: "At Pets-R-Us we take serious care of every pet that boards with us.  When you go on vacation or need to leave your pet for a period of time you don’t want any stress or concern about your pet.  With our boarding services each pet will receive individualized care based on their needs.  Each pet will have daily activities to keep them active and exercising.  Each will receive any individualized care they need whether it be medicine they need to take or special diets they need to maintain while you are away.  All of our boarding and activity rooms have 24/7 cameras so every customer can check up on their loved one at any time of day.  The pets in our care will receive 24/7 supervison as we have employees working at all hours of the morning, day, and night.  We take pride in having each pet being looked after and having all their needs met and that what your pet will get when boarding with Pets-R-Us.  ",
         heroImage: "images/panda.png",
-        boardingItems: ["1 dog up to 2 nights: $40/night","2 dog up to 2 nights: $30/night", "3 dog up to 2 nights: $20/night", "4 dog up to 2 nights: $10/night"],
+        boardingItems: ["1 dog up to 2 nights: $40/night", "2 dog up to 2 nights: $30/night", "3 dog up to 2 nights: $20/night", "4 dog up to 2 nights: $10/night"],
         boardingHeading: "Our Boarding",
         boardingDescription: "Our boarding offers a safe and secure environment for dogs while their owners are away."
     })
 })
 
+// setting up route for register page and passing the relevant data to the register file
 app.get('/register', (req, res) => {
     res.render('register', {
         navItems,
@@ -174,6 +179,27 @@ app.get('/register', (req, res) => {
 
     })
 })
+
+// setting up route for customers page and passing the relevant data to the customer file
+app.get('/customers', (req, res) => {
+    // using mongoose find method to fidn all the customers from MongoDB and passing them to customers-list page.
+    Customer.find({}, function (err, customers) {
+        if (err) {
+            console.log(err);
+            next(err);
+        } else {
+            res.render('customer-list', {
+                navItems,
+                title: 'Pets R Us',
+                description: "At Pets-R-Us we take serious care of every pet that boards with us.  When you go on vacation or need to leave your pet for a period of time you don’t want any stress or concern about your pet.  With our boarding services each pet will receive individualized care based on their needs.  Each pet will have daily activities to keep them active and exercising.  Each will receive any individualized care they need whether it be medicine they need to take or special diets they need to maintain while you are away.  All of our boarding and activity rooms have 24/7 cameras so every customer can check up on their loved one at any time of day.  The pets in our care will receive 24/7 supervison as we have employees working at all hours of the morning, day, and night.  We take pride in having each pet being looked after and having all their needs met and that what your pet will get when boarding with Pets-R-Us.  ",
+                customers: customers
+            })
+        }
+    })
+
+       
+})
+
 // handling the post rqequest
 app.post('/register', (req, res, next) => {
     // creating a new user by validating it against the CUstomer schema
@@ -183,12 +209,12 @@ app.post('/register', (req, res, next) => {
     })
 
     // creating a new customer to mongoDB
-    Customer.create(newCustomer, function(err, customer) {
+    Customer.create(newCustomer, function (err, customer) {
         if (err) {
             console.log(err);
             next(err);
         } else {
-            res.render('index',homeData)
+            res.render('index', homeData)
         }
     })
 })
